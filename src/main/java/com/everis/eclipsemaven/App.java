6b9e7888ac46a9a43aded06153dd4fb2c3185a1d
Everis Formación <everis.formacion.svq@gmail.com>
2021-03-15 14:32:07 +0100

    Ejemplo 1 - AM

---
+++ src/main/java/com/everis/eclipsemaven/App.java
@@ -0,0 +1,18 @@
+package com.everis.eclipsemaven;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		if(NumberUtils.compare(10, 10) == 0) {
+			System.out.println("SI, SON IGUALES");
+		}
+	}
+}
