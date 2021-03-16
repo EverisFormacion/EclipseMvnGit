c86304433fcf56f6349ff098961a094ab442b4ba
Everis Formación <everis.formacion.svq@gmail.com>
2021-03-16 09:50:04 +0100

    Ejemplo 6 - AM - EVO

--- src/main/java/com/everis/eclipsemaven/App.java
+++ src/main/java/com/everis/eclipsemaven/App.java
@@ -9,11 +9,10 @@ import org.apache.commons.lang3.math.NumberUtils;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("¡HOLA BOOTCAMP 15/03!");
+		System.out.println("¡HOLA BOOTCAMP 15/03! - EVO");
 
-		System.out.println("¡HOLA BOOTCAMP 15/03!");
+		System.out.println("¡HOLA BOOTCAMP 15/03! - EVO");
 
-		System.out.println("¡HOLA BOOTCAMP 15/03!");
-
+		System.out.println("¡HOLA BOOTCAMP 15/03! - EVO");
 	}
 }
